0c1b69c9b67eb0b8570c53cfd27207c1a91ff7ef
Karim Khaleel <code.karim@gmail.com>
2023-05-29 08:55:46 +0300

    feat(plugins): change vim-surround to nvim-surround

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -72,7 +72,28 @@ return {
       vim.g.matchup_matchparen_offscreen = { method = "popup" }
     end,
   },
-  { "tpope/vim-surround", event = "BufReadPre" },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        keymaps = {
+          insert = "<C-g>s",
+          insert_line = "<C-g>S",
+          normal = "ys",
+          normal_cur = "yss",
+          normal_line = "yS",
+          normal_cur_line = "ySS",
+          visual = "<Leader>S",
+          visual_line = "gS",
+          delete = "ds",
+          change = "cs",
+          change_line = "cS",
+        },
+      }
+    end,
+  },
   {
     "numToStr/Comment.nvim",
     dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
